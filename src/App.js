4327a2c22e93430ddddddd1aae34c47f8be7873f
unknown <emonhowlader1997@gmail.com>
2022-06-25 11:57:12 +0600

    navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import Navbar from './components/Home/Navbar/Navigation';
+import Home from './components/Home/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      welcome travel guru
+    <div className="App"> 
+      <Home />     
     </div>
   );
 }
---
+++ src/components/Home/Navbar/Navigation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+import logo from '../../../Assets/Logo.png'
+
+const Navigation = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+  <Container>
+  <Navbar.Brand href="#home">
+      <img src={logo} className="img-fluid" height='130px' width='150px' alt="" />
+  </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <div className="search-bar ms-auto">
+      <input type="text" placeholder='Search your destination' className='form-control' />
+    </div>
+    <Nav className="ms-auto">
+      <Nav.Link href="#features">News</Nav.Link>
+      <Nav.Link href="#pricing">Destination</Nav.Link> 
+      <Nav.Link href="#pricing">Blog</Nav.Link> 
+      <Nav.Link href="#pricing">Contact</Nav.Link> 
+      <button className='btn btn-warning'>Login</button> 
+    </Nav> 
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+  );
+};
+
+export default Navigation;
